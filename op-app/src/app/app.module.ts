32dcc859b948a8796ebcdd1050d65827e68ebe38
Ichai Gutierrez <ichai.gutierrez@gmail.com>
2022-06-15 11:58:28 -0700

    Material.angular
    Se instalo el paquete material.angular via CLI

--- op-app/src/app/app.module.ts
+++ op-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginAdminComponent } from './login-admin/login-admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginAdminComponent } from './login-admin/login-admin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
