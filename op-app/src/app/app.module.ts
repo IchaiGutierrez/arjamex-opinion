c9ca21cbb437559730200ba7b8ae5b99e4b7c230
Daniel Mendoza <xdamm777@gmail.com>
2022-06-17 11:56:21 -0700

    Added user registration component

--- op-app/src/app/app.module.ts
+++ op-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginAdminComponent } from './login-admin/login-admin.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LoginAdminComponent
+    LoginAdminComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
