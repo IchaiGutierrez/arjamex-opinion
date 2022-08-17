ad245e6680fb1405b5e096a2cd6866230d9b515d
Daniel Mendoza <xdamm777@gmail.com>
2022-08-17 11:17:56 -0700

    Added token storage service

---
+++ op-app/src/app/services/token-storage.service.ts
@@ -0,0 +1,39 @@
+//manages token and user info (username, roles) in session storage and clears on logout
+import { Injectable } from '@angular/core';
+import { userInfo } from 'os';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+  constructor() { }
+
+  signOut(): void {
+    window.sessionStorage.clear();
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string | null {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    const user = window.sessionStorage.getItem(USER_KEY)
+    if (user) {
+      return JSON.parse(user);
+    }
+    return {};
+  }
+}
